b48ce8b7bf1935d3d2fc3e9e8fffedec2a574d6b
zweeltow <glebbygaev@gmail.com>
2020-06-19 10:36:55 +0300

    Added support geolocation.

--- zweelqrbot.py
+++ zweelqrbot.py
@@ -8,70 +8,83 @@ import os
 bot = telebot.TeleBot(config.TOKEN)
 
 
-def log(message):
-    log_message_none = f'[<a href="tg://user?id={message.from_user.id}">{message.from_user.id}</a>] {message.from_user.first_name} "{message.from_user.username}": {message.text}'
-    log_message = f'[<a href="tg://user?id={message.from_user.id}">{message.from_user.id}</a>] {message.from_user.first_name} "{message.from_user.username}" {message.from_user.last_name}: {message.text}'
-    bot.send_message(config.logchannelid, f'{message}')
-    bot.send_chat_action(message.chat.id, 'typing')
-    if message.from_user.last_name == None:
-        bot.send_message(config.logpm, log_message_none, disable_web_page_preview=None,
-                         reply_to_message_id=None, reply_markup=None, parse_mode='html', disable_notification=True)
-    else:
-        bot.send_message(config.logpm, log_message, disable_web_page_preview=None,
-                         reply_to_message_id=None, reply_markup=None, parse_mode='html', disable_notification=True)
+def log(message, handler):
+    bot.send_message(config.CHANNEL, f'{message}')
 
+    if handler == "text":
+        bot.send_message(config.OWNER, f'[<a href="tg://user?id={message.from_user.id}">{message.from_user.id}</a>] {message.from_user.first_name} "{message.from_user.username}" {message.from_user.last_name}: {message.text}',
+                        parse_mode='html', disable_notification=True)   
+    elif handler == "sticker":
+        bot.send_message(config.OWNER, f'[<a href="tg://user?id={message.from_user.id}">{message.from_user.id}</a>] {message.from_user.first_name} "{message.from_user.username}" {message.from_user.last_name}:', 
+                        parse_mode='html', disable_notification=True)
+        bot.send_sticker(config.OWNER, message.sticker.file_id, disable_notification=True)
+    
 
-@bot.message_handler(commands=['start'])
-def start_hello(message):
-    log(message)
-    if message.from_user.last_name == None:
-        bot.send_message(
-            message.chat.id, f'Привет, {message.from_user.first_name}!\n\nЭтот бот преобразовает текст и стикеры в <a href="https://ru.wikipedia.org/wiki/QR-%D0%BA%D0%BE%D0%B4">QR-код</a>.', parse_mode='html', disable_web_page_preview=False)
-    else:
-        bot.send_message(
-            message.chat.id, f'Привет, {message.from_user.first_name} {message.from_user.last_name}!\n\nЭтот бот создает QR-код. Просто нипиши ему что-нибудь. Или отправь стикер, чтобы получить информацию о нем.')
+    return handler
 
 
 def qr_gen(message, handler):
-    log(message)
+    log(message, handler)
 
     if handler == "text":
         loc = message.text
     elif handler == "sticker":
         loc = message.sticker
+    elif handler == "location":
+        loc = f"geo:{message.location.latitude},{message.location.longitude}"
 
-    try:
-        qr = qrcode.make(loc)
-        name_qrcode = random.randrange(99)
-        qr.save(f"/var/www/pybot/qrbot/qr/{name_qrcode}.png")
-        qrpic = open(f"/var/www/pybot/qrbot/qr/{name_qrcode}.png", 'rb')
-        bot.send_chat_action(message.chat.id, 'upload_photo')
-        bot.send_photo(message.chat.id, qrpic,
-                       reply_to_message_id=message.message_id)
-        os.remove(f'/var/www/pybot/qrbot/qr/{name_qrcode}.png')
-    except Exception as error:
-        bot.send_message(message.chat.id, "Произошла ошибка. :(")
-        bot.send_message(config.logpm, f"Произошла ошибка!:\n{error}")
+    qr = qrcode.make(loc)
+    name_qrcode = random.randrange(99)
+    qr.save(f"/var/www/pybot/qrbot/qr/{name_qrcode}.png")
+    qrpic = open(f"/var/www/pybot/qrbot/qr/{name_qrcode}.png", 'rb')
+    bot.send_chat_action(message.chat.id, 'upload_photo')
+    bot.send_photo(message.chat.id, qrpic,
+                    reply_to_message_id=message.message_id)
+    os.remove(f'/var/www/pybot/qrbot/qr/{name_qrcode}.png')
         
     return handler
 
 
+@bot.message_handler(commands=['start'])
+def start_hello(message):
+    log(message, "text")
+
+    if message.from_user.language_code == 'ru':
+        bot.send_message(
+                message.chat.id, f'👋 Привет, {message.from_user.first_name}!\nЭтот бот преобразовает текст и геолокацию в <a href="https://ru.wikipedia.org/wiki/QR-%D0%BA%D0%BE%D0%B4">QR код</a>.', parse_mode='html')
+    else:
+        bot.send_message(
+            message.chat.id, f'👋 Hey, {message.from_user.first_name}!\nThis bot converts text and geolocation to the <a href="https://en.wikipedia.org/wiki/QR_code">QR code</a>.', parse_mode='html')
+
+
 @bot.message_handler(content_types=['text'])
-def send_txt(message):
+def qr_text(message):
     try:
         qr_gen(message, "text")
-    except Exception as limit:
-        bot.send_message(message.chat.id, f"Лимит: {limit}")
+    except Exception as error:
+        bot.send_message(message.chat.id, f"😔 Error: {error}")
+        bot.send_message(config.OWNER, f"❗️Произошла ошибка:\n{error}")
 
 
 @bot.message_handler(content_types=['sticker'])
-def send_sti(message):
+def qr_sticker(message):
     try:
         qr_gen(message, "sticker")
-    except Exception as limit:
-        bot.send_message(message.chat.id, f"Лимит: {limit}")
+    except Exception as error:
+        bot.send_message(message.chat.id, f"😔 Error: {error}")
+        bot.send_message(config.OWNER, f"❗️Произошла ошибка:\n{error}")
+
+
+@bot.message_handler(content_types=['location'])
+def qr_location(message):
+    try:
+        qr_gen(message, "location")
+    except Exception as error:
+        bot.send_message(message.chat.id, f"😔 Error: {error}")
+        bot.send_message(config.OWNER, f"❗️Произошла ошибка:\n{error}")
+
 
 try:
     bot.infinity_polling()
 except Exception as e:
-    bot.send_message(config.logpm, f"Ошибка в запросе:\n{e}")
+    bot.send_message(config.OWNER, f"❗️Ошибка в запросе:\n{e}")
