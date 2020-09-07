e6cecd5bf67d6600361b5aa78d3f8293237ae9fe
zweeltow <glebbygaev@gmail.com>
2020-09-07 17:53:36 +0300

    я уже говорил, что кодить не умею?

--- zweelqrbot.py
+++ zweelqrbot.py
@@ -16,10 +16,10 @@ dir_pictures = '' # dir where pictures will be created
 def log(message, handler):
     bot.send_message(config.CHANNEL, message)
 
-    if handler == "text":
+    if handler == message.text:
         bot.send_message(config.OWNER, f'[<a href="tg://user?id={message.from_user.id}">{message.from_user.id}</a>] {message.from_user.first_name} "{message.from_user.username}" {message.from_user.last_name}: {message.text}',
                         parse_mode='html', disable_notification=True)   
-    elif handler == "sticker":
+    elif handler == message.sticker:
         bot.send_message(config.OWNER, f'[<a href="tg://user?id={message.from_user.id}">{message.from_user.id}</a>] {message.from_user.first_name} "{message.from_user.username}" {message.from_user.last_name}:', 
                         parse_mode='html', disable_notification=True)
         bot.send_sticker(config.OWNER, message.sticker.file_id, disable_notification=True)
@@ -32,38 +32,38 @@ def qr_gen(message, handler):
     name_qrcode_file = random.randrange(99)
     qr.save(f"{dir_pictures}/{name_qrcode_file}.png")
     qr_pic = open(f"{dir_pictures}/{name_qrcode_file}.png", 'rb')
+
     bot.send_chat_action(message.chat.id, 'upload_photo')
-    bot.send_photo(message.chat.id, qr_pic,
-                    reply_to_message_id=message.message_id)
+    bot.send_photo(message.chat.id, qr_pic, reply_to_message_id=message.message_id)
+
     os.remove(f'{dir_pictures}/{name_qrcode_file}.png')
 
 
 @bot.message_handler(commands=['start'])
 def start_hello(message):
-    handler = "text"
+    handler = message.text
     log(message, handler)
 
     if message.from_user.language_code == 'ru':
-        bot.send_message(
-                message.chat.id, f'👋 Привет, {message.from_user.first_name}!\nЭтот бот преобразовает текст и геолокацию в <a href="https://ru.wikipedia.org/wiki/QR-%D0%BA%D0%BE%D0%B4">QR-код</a>.', parse_mode='html')
+        text = f'👋 Привет, {message.from_user.first_name}!\nЭтот бот преобразовает текст и геолокацию в <a href="https://ru.wikipedia.org/wiki/QR-%D0%BA%D0%BE%D0%B4">QR-код</a>.'
     else:
-        bot.send_message(
-            message.chat.id, f'👋 Hey, {message.from_user.first_name}!\nThis bot converts text and location to <a href="https://en.wikipedia.org/wiki/QR_code">QR code</a>.', parse_mode='html')
+        text = f'👋 Hey, {message.from_user.first_name}!\nThis bot converts text and location to <a href="https://en.wikipedia.org/wiki/QR_code">QR code</a>.'
+
+    bot.send_message(message.chat.id, text, parse_mode='html')
 
 
 @bot.message_handler(commands=['feedback'])
 def leave_feedback(message):
-    handler = "text"
+    handler = message.text
     log(message, handler)
 
     if message.from_user.language_code == 'ru':
-        bot.send_message(message.chat.id, 
-                            '💬 Пожалуйста, расскажите о Ваших <b>пожеланиях</b> или <b>проблемах</b>, с которыми Вы столкнулись, используя бота.\n\nИспользуйте /cancel, чтобы отменить команду.\n\nЕсли Вы желаете диалога с разработчиком, то возможно общение в личной переписке: @zweel.', parse_mode='html')
-        bot.register_next_step_handler(message, get_feedback)
+        text = '💬 Пожалуйста, расскажите о Ваших <b>пожеланиях</b> или <b>проблемах</b>, с которыми Вы столкнулись, используя бота.\n\nИспользуйте /cancel, чтобы отменить команду.\n\nЕсли Вы желаете диалога с разработчиком, то возможно общение в личной переписке: @zweel.'
     else:
-        bot.send_message(message.chat.id, 
-                        "💬 Please tell me about your <b>suggestions</b> or <b>problems</b> that you encountered using the bot.\n\nUse /cancel to cancel this command.\n\nIf you need a dialogue with the developer, you can PM: @zweel. <i>(But I'm bad at English 🙂)</i>.", parse_mode='html')
-        bot.register_next_step_handler(message, get_feedback)
+        text = "💬 Please tell me about your <b>suggestions</b> or <b>problems</b> that you encountered using the bot.\n\nUse /cancel to cancel this command.\n\nIf you need a dialogue with the developer, you can PM: @zweel. <i>(But I'm bad at English 🙂)</i>"
+
+    bot.send_message(message.chat.id, text, parse_mode='html')
+    bot.register_next_step_handler(message, get_feedback)
 
 def get_feedback(message):
     if message.text == '/cancel':
@@ -74,26 +74,30 @@ def get_feedback(message):
         bot.forward_message(config.OWNER, message.chat.id, message.message_id)
         
         if message.from_user.language_code == 'ru':
-            bot.send_message(message.chat.id, 'Отлично! Ваше сообщение отправлено.', reply_to_message_id=message.message_id)
+            text = 'Отлично! Ваше сообщение отправлено.'
         else:
-            bot.send_message(message.chat.id, 'Awesome! Your message has been sent.', reply_to_message_id=message.message_id)
+            text = 'Awesome! Your message has been sent.'
+
+        bot.send_message(message.chat.id, text, reply_to_message_id=message.message_id)
 
 
 @bot.message_handler(commands=['help'])
 def msg_help(message):
-    handler = "text"
+    handler = message.text
     log(message, handler)
 
     if message.from_user.language_code == 'ru':
-        bot.send_message(message.chat.id, '🤖 Данный бот конвертирует <b>текст</b> и <b>геолокацию</b> в <a href="https://ru.wikipedia.org/wiki/QR-%D0%BA%D0%BE%D0%B4">QR-код</a>. \n\nБот написан на Python и использует библиотеки: \n• <a href="https://github.com/eternnoir/pyTelegramBotAPI">pyTelegramBotApi</a> — для общения с <a href="https://core.telegram.org/bots/api">Telegram Bot API</a>.\n• <a href="https://github.com/lincolnloop/python-qrcode">qrcode</a> — для создания QR-кодов.\n\n<b>Команды:</b>\n/feedback — команда на случай изъявления желания оставить пожелания разработчику или сообщить об ошибке.\n/about — получить информацию о боте.', parse_mode="html", disable_web_page_preview=True)
+        text = '🤖 Данный бот конвертирует <b>текст</b> и <b>геолокацию</b> в <a href="https://ru.wikipedia.org/wiki/QR-%D0%BA%D0%BE%D0%B4">QR-код</a>. \n\nБот написан на Python и использует библиотеки: \n• <a href="https://github.com/eternnoir/pyTelegramBotAPI">pyTelegramBotApi</a> — для общения с <a href="https://core.telegram.org/bots/api">Telegram Bot API</a>.\n• <a href="https://github.com/lincolnloop/python-qrcode">qrcode</a> — для создания QR-кодов.\n\n<b>Команды:</b>\n/feedback — команда на случай изъявления желания оставить пожелания разработчику или сообщить об ошибке.\n/help — получить информацию о боте.'
     else:
-        bot.send_message(message.chat.id, '🤖 This bot converts <b>text</b> and <b>geolocation</b> to <a href="https://en.wikipedia.org/wiki/QR_code">QR code</a>.\n\nThe bot is written in Python and uses the following libraries:\n• <a href="https://github.com/eternnoir/pyTelegramBotAPI">pyTelegramBotApi</a> — for communication with the <a href="https://core.telegram.org/bots/api">Telegram Bot API</a>.\n• <a href="https://github.com/lincolnloop/python-qrcode">qrcode</a> — to create QR codes.\n\n<b>Commands:</b>\n/feedback — leave feedback about the bot.\n/help — get information about the bot.', parse_mode='html', disable_web_page_preview=True)
+        text = '🤖 This bot converts <b>text</b> and <b>geolocation</b> to <a href="https://en.wikipedia.org/wiki/QR_code">QR code</a>.\n\nThe bot is written in Python and uses the following libraries:\n• <a href="https://github.com/eternnoir/pyTelegramBotAPI">pyTelegramBotApi</a> — for communication with the <a href="https://core.telegram.org/bots/api">Telegram Bot API</a>.\n• <a href="https://github.com/lincolnloop/python-qrcode">qrcode</a> — to create QR codes.\n\n<b>Commands:</b>\n/feedback — leave feedback about the bot.\n/help — get information about the bot.'
+
+    bot.send_message(message.chat.id, text, parse_mode='html', disable_web_page_preview=True)
 
 
 @bot.message_handler(content_types=['text'])
 def qr_text(message):
     try:
-        handler = "text"
+        handler = message.text
         qr_gen(message, handler)
     except Exception as error:
         bot.send_message(message.chat.id, f"😔 Error: {error}")
@@ -103,21 +107,25 @@ def qr_text(message):
 @bot.message_handler(content_types=['sticker'])
 def qr_sticker(message):
     try:
-        handler = 'sticker'
+        handler = message.sticker
         qr_gen(message, handler)
     except Exception as error:
         bot.send_message(message.chat.id, f"😔 Error: {error}")
         bot.send_message(config.OWNER, f"❗️Произошла ошибка:\n{error}")
 
 
-@bot.message_handler(content_types=['location'])
+@bot.message_handler(content_types=['location', 'venue'])
 def qr_location(message):
     try:
-        handler = "location"
+        if message.location:
+            handler = f"geo:{message.location.latitude},{message.location.longitude}"
+        elif message.venue:
+            handler = f"geo:{message.venue.location.latitude},{message.venue.location.longitude}"
         qr_gen(message, handler)
     except Exception as error:
         bot.send_message(message.chat.id, f"😔 Error: {error}")
         bot.send_message(config.OWNER, f"❗️Произошла ошибка:\n{error}")
+
 
 @bot.message_handler(content_types=['photo', 'video', 'document', "audio"])
 def unknown(message):
